8e6ab6003d99758f37a4440ded4fe9d2b2d55822
codejstn00 <justinjamesalviar@gmail.com>
2026-06-18 01:51:19 +0800

    change title and icon

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+const link = document.querySelector("link[rel~='icon']") || document.createElement('link');
+link.rel = 'icon';
+link.href = 'data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><rect width=%22100%22 height=%22100%22 rx=%2220%22 fill=%22%234f46e5%22/><text y=%2270%22 x=%2218%22 fill=%22white%22 font-family=%22sans-serif%22 font-size=%2255%22 font-weight=%22bold%22>SP</text></svg>';
+document.getElementsByTagName('head')[0].appendChild(link);
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
